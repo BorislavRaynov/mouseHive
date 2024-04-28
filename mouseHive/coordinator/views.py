fe06147c066824d6170555754826e447951bc0ca
BorislavRaynov <b_raynov@abv.bg>
2024-04-28 18:26:11 +0300

    home page logic added

--- mouseHive/coordinator/views.py
+++ mouseHive/coordinator/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import MouseCoordinate
 
 
 def show_coordinates(request):
+
+    # latest_coordinates = MouseCoordinate.objects.last()
+    #
+    # context = {
+    #     'latest_coordinates': latest_coordinates
+    # }
+    #
+    # return render(request, 'coordinator/home-page.html', context=context)
     return render(request, 'coordinator/home-page.html')
