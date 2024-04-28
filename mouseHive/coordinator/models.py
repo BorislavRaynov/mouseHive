8303403db1f3a6f27af14f357b1b804811e0e4a2
BorislavRaynov <b_raynov@abv.bg>
2024-04-28 16:48:48 +0300

    coordinate model added

--- mouseHive/coordinator/models.py
+++ mouseHive/coordinator/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class MouseCoordinate(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Mouse Coordinate ({self.x}, {self.y}) at {self.timestamp}"
+
+
+class ImageDataSource(models.Model):
+    image = models.ImageField(upload_to='images/')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image Data Source uploaded at {self.timestamp}"
